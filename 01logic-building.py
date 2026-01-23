1546f2731fe229b127c82414e020299343967522
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-01-23 23:36:20 +0500

    table printing patten working just fine

--- 01logic-building.py
+++ 01logic-building.py
@@ -4,7 +4,7 @@
 # Draw Observations: Draw observations and patterns based on the examples you created.
 # Start with Basic: First, think of the basic way to solve a problem 
 
-#Check even or odd 
+#Given a number n, check whether it is even or odd. Return true for even and false for odd.
 number = int(input("Enter a Number: "))
 
 def findEvenOdd():
@@ -15,3 +15,7 @@ def findEvenOdd():
 
 result = findEvenOdd()
 print(result)
+
+# Multiplication Table
+for i in range(1, 11):
+    print(f"{number} x {i} = {number*i}")
