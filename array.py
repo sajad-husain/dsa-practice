80048de68d9454276f8cb66219e1153d26434071
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-02-05 10:53:14 +0500

    learned typecode and array reversing methods

--- array.py
+++ array.py
@@ -8,8 +8,21 @@ print(val) #print whole array
 # iterate on each element use for loop
 for i in range(0, 5):
     print("simple loop",val[i], end=" ") #endl for adding spaces
-    
+
 print('\n')
-# enhcaned for loop
+# taking simple array to next level
+for i in range(0, len(val)):
+    print(val[i])
+
+print('\n')
+# enhcaned for loop if array size changes than simple loop wont be efficent
 for x in val:
     print("enhanced loop",x)
+
+print('\n')
+print("checking type code", val.typecode)
+
+print("reversing the array")
+val.reverse()
+for i in val:
+    print(i, end=" ")
