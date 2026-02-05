0d7b5e74a94a4fc2d51b148938edcf526a6acfa2
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-02-05 10:30:39 +0500

    iterating through arrays usign for loop and enhanced for loop

--- array.py
+++ array.py
@@ -1,3 +1,12 @@
 import array
 
-val = array.array()
+val = array.array('i', [1,2,4,5,6])
+print(val) #print whole array
+
+# iterate on each element use for loop
+for i in range(0, 6):
+    print("simple loop",val[i], end=" ") #endl for adding spaces
+    
+# enhcaned for loop
+for x in val:
+    print("enhanced loop",x)
