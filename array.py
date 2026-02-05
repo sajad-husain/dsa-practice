0abab33074c6accf727219f52da2f64a9c7ee31d
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-02-05 10:40:15 +0500

    debugged and resolved issues

--- array.py
+++ array.py
@@ -4,7 +4,7 @@ val = array.array('i', [1,2,4,5,6])
 print(val) #print whole array
 
 # iterate on each element use for loop
-for i in range(0, 6):
+for i in range(0, 5):
     print("simple loop",val[i], end=" ") #endl for adding spaces
     
 # enhcaned for loop
