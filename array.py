ad0287ab25a36bc29497338e86abb54edeb880a8
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-02-05 10:56:44 +0500

    insert and append methods learned

--- array.py
+++ array.py
@@ -5,24 +5,28 @@ from array import * #perfect we'll be using module directly
 val = array('i', [1,2,4,5,6])
 print(val) #print whole array
 
-# iterate on each element use for loop
-for i in range(0, 5):
-    print("simple loop",val[i], end=" ") #endl for adding spaces
+# # iterate on each element use for loop
+# for i in range(0, 5):
+#     print("simple loop",val[i], end=" ") #endl for adding spaces
 
-print('\n')
-# taking simple array to next level
-for i in range(0, len(val)):
-    print(val[i])
+# print('\n')
+# # taking simple array to next level
+# for i in range(0, len(val)):
+#     print(val[i])
 
-print('\n')
-# enhcaned for loop if array size changes than simple loop wont be efficent
-for x in val:
-    print("enhanced loop",x)
+# print('\n')
+# # enhcaned for loop if array size changes than simple loop wont be efficent
+# for x in val:
+#     print("enhanced loop",x)
 
-print('\n')
+# print('\n')
 print("checking type code", val.typecode)
 
 print("reversing the array")
 val.reverse()
+
+print("inserting an element in array", val.insert(1, 90))
+print("adding elemet in end", val.append(121))
+
 for i in val:
     print(i, end=" ")
