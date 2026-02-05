029c8a7708e62645ba2dd15e13aea0546539d713
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-02-05 10:44:43 +0500

    New imports used to factorize the code properly

--- array.py
+++ array.py
@@ -1,12 +1,15 @@
-import array
+# import array
+# import array as Arr1 # Alias for array 
+from array import * #perfect we'll be using module directly 
 
-val = array.array('i', [1,2,4,5,6])
+val = array('i', [1,2,4,5,6])
 print(val) #print whole array
 
 # iterate on each element use for loop
 for i in range(0, 5):
     print("simple loop",val[i], end=" ") #endl for adding spaces
     
+print('\n')
 # enhcaned for loop
 for x in val:
     print("enhanced loop",x)
