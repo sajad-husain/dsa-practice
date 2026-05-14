a9d1afaaa0936964515b1ce76c09145f4ccaf953
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-05-14 10:39:32 +0500

    created a list and accessed it's elements using Indexing methods

--- 02_data_strucutres.py
+++ 02_data_strucutres.py
@@ -6,7 +6,7 @@ mix_list = [1, "Junaid", True, None]
 
 print(f"Empty List {empty_list}, \nFilled List: {list_with_data}, \nList with mixed data: {mix_list}")
 
-#using list constructor
+# using list constructor
 constructor_list = list("Ali")
 print(constructor_list)
 print(list("SAJJAD")) # alternative way to write a list
@@ -14,3 +14,13 @@ print(list("SAJJAD")) # alternative way to write a list
 #range based lists
 ranged_list = list(range(6))
 print("Ranged list: ", ranged_list)
+
+# Accessing elements in lists
+fruits = ["Apple", "Oranges", "Cherry", "Banana"]
+prin
+
+# Indexing based
+print(fruits[2])
+print(fruits[0])
+print(fruits[-1]) # last element
+print(fruits[-2]) # second last element
