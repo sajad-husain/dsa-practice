cf5ac15b8f170eee98a7b1d422729b49cb9c1d64
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-05-14 14:16:38 +0500

    learned about lambda funciton

--- 02_data_strucutres.py
+++ 02_data_strucutres.py
@@ -56,43 +56,62 @@
 # print("Deleted now list looks like this", numbers)
 
 # LIST METHODS
-number_list = [3,1,4,1,5,1,9,2,6]
+# number_list = [3,1,4,1,5,1,9,2,6]
 
-number_list.append(121) # adding at last of the list
-print("list after appending", number_list)
+# number_list.append(121) # adding at last of the list
+# print("list after appending", number_list)
 
-number_list.extend([11,12]) # add multiple items
-print("list after extending", number_list)
+# number_list.extend([11,12]) # add multiple items
+# print("list after extending", number_list)
 
-number_list.insert(0,100) # adding hundred at 0th index
-print("list after inserting", number_list)
+# number_list.insert(0,100) # adding hundred at 0th index
+# print("list after inserting", number_list)
 
-number_list.remove(1) # remove at 0 index
-print("list after removing the item",number_list)
+# number_list.remove(1) # remove at 0 index
+# print("list after removing the item",number_list)
 
-last = number_list.pop() # remove and return last value of the list 
-print(last)
-print("Popping last value", number_list)
+# last = number_list.pop() # remove and return last value of the list 
+# print(last)
+# print("Popping last value", number_list)
 
-second = number_list.pop(1) # remove the value at index 1
-print(second)
-print("Popping value at the index", number_list)
+# second = number_list.pop(1) # remove the value at index 1
+# print(second)
+# print("Popping value at the index", number_list)
 
-counted_value = number_list.count(1) # how much time 1 is appeared
-print(f"this value appeared {counted_value} times")
+# counted_value = number_list.count(1) # how much time 1 is appeared
+# print(f"this value appeared {counted_value} times")
 
-index_8 = number_list.index(1) # on which index 1 exists
-print(f"on {index_8} index value appears first")
+# index_8 = number_list.index(1) # on which index 1 exists
+# print(f"on {index_8} index value appears first")
 
-number_list.sort()
-print(f"Sorted List {number_list}")
+# number_list.sort()
+# print(f"Sorted List {number_list}")
 
-number_list.reverse()
-print(f"reversed list {number_list}")
+# number_list.reverse()
+# print(f"reversed list {number_list}")
+
+
+# Lambda chota function hota h jo 1 line pe likha jata h iska return type ni hota 
+# def add(a,b):
+#     return a + b
+
+# lambda argument: expression
+add = lambda a , b: a + b
+print(add(5,3))
+
+num_list = [1,2,3,4,5,6]
+square = list(map(lambda x: x*x, num_list))
+print("Mapping and Squaring: ", square)
+
+filter = list(filter(lambda x: x % 2 == 0, num_list ))
+print("Filtered list ", filter)
 
 student = [
     {"name": "Alice", "grade": 85},
     {"name": "Ling Shao", "grade": 15},
     {"name": "Proton", "grade": 65}
 ]
+
+student.sort(key=lambda x:x['grade'])
+print(student)
 
