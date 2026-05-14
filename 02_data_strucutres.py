d93851c40c658be7bee4c4f7b91e08bcea96976f
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-05-14 10:32:54 +0500

    created a list

---
+++ 02_data_strucutres.py
@@ -0,0 +1,7 @@
+empty_list = []
+
+list_with_data = [1,2,3,4,5]
+
+mix_list = [1, "Junaid", True, None]
+
+print(f"Empty List {empty_list}, \nFilled List: {list_with_data}, \nList with mixed data: {mix_list}")
