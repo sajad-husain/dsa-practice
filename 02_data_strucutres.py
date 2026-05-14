429fa3fa40df41eee27300a851081efdba4d93fa
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-05-14 10:35:57 +0500

    ceated new lists using contructor and ranged method

--- 02_data_strucutres.py
+++ 02_data_strucutres.py
@@ -5,3 +5,12 @@ list_with_data = [1,2,3,4,5]
 mix_list = [1, "Junaid", True, None]
 
 print(f"Empty List {empty_list}, \nFilled List: {list_with_data}, \nList with mixed data: {mix_list}")
+
+#using list constructor
+constructor_list = list("Ali")
+print(constructor_list)
+print(list("SAJJAD")) # alternative way to write a list
+
+#range based lists
+ranged_list = list(range(6))
+print("Ranged list: ", ranged_list)
