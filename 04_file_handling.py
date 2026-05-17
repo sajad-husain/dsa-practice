ef41fc36c9291ea4f0e5f5fb1414cde6340d5d28
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-05-17 13:20:30 +0500

    writing files in python

--- 04_file_handling.py
+++ 04_file_handling.py
@@ -12,3 +12,16 @@ with open("data.txt", "r") as file:
 with open("data.txt", "r") as file:
     lines = file.readlines()
     print(lines)
+    
+# WRITING FILES IN PYTHON
+with open("output.txt", "w") as file:
+    file.write("Hell0 there!\n")
+    
+# Append to file
+with open("output.txt", "a") as file:
+    file.write("this is new text")
+
+# write multiple files
+new_lines = ["line 1", "line 2", "line 3"]
+with open("output.txt", "w") as file:
+    file.writelines(f"{new_lines}\n")
