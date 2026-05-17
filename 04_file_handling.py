44bc7b28bee56a0f0084be337e27b2937f287133
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-05-17 13:11:32 +0500

    reading text file

--- 04_file_handling.py
+++ 04_file_handling.py
@@ -2,3 +2,8 @@
 with open("data.txt", "r") as file:
     content = file.read()
     print(content)
+
+# Read line by line
+with open("data.txt", "r") as file: 
+    for line in file:
+        print(line.strip())
