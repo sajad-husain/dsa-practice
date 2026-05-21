d2168181f70fcd959d3f66259985b1430e763223
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-05-21 23:58:43 +0500

    demonstrate constructor usage in python

--- oop.py
+++ oop.py
@@ -4,3 +4,15 @@ class Car:
 
 my_car = Car()
 my_car.start()
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+
+s1 = Student("Ali", 20)
+s1.info()
