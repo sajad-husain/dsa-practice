6c21dbde9027d0363063a8a82b395b5ce31b2756
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-05-21 23:59:07 +0500

    add encapsulation example with private variable

--- oop.py
+++ oop.py
@@ -16,3 +16,18 @@ class Student:
 
 s1 = Student("Ali", 20)
 s1.info()
+
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def show_balance(self):
+        print("Balance:", self.__balance)
+
+
+account = BankAccount(1000)
+account.deposit(500)
+account.show_balance()
